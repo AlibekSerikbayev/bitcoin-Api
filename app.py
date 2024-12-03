a22f5f7bf10cdcd619a50cadd83b0d3c16878c74
AlibekSerikbayev <aliserikbek55@gmail.com>
2024-12-03 14:16:25 +0500

    app 5

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import pandas as pd
-#uzgarish 3
+
 # API orqali ma'lumot olish funksiyasi
 def get_bitcoin_data(api_key):
     url = "https://api.coincap.io/v2/assets/bitcoin"
@@ -37,13 +37,64 @@ if api_key:
             ]
         }
 
+        # Jadval ko'rinishida chiqarish
         df = pd.DataFrame(data)
-        st.table(df)  # Jadvalda ko'rsatish
+        st.write(df)  # Jadvalda ko'rsatish
 
     else:
         st.error("Ma'lumotlarni olishda xatolik yuz berdi. API kalitni tekshiring.")
 else:
     st.info("Ma'lumotlarni ko'rish uchun API kalitni kiriting.")
+
+
+
+
+# import streamlit as st
+# import requests
+# import pandas as pd
+# #uzgarish 3
+# # API orqali ma'lumot olish funksiyasi
+# def get_bitcoin_data(api_key):
+#     url = "https://api.coincap.io/v2/assets/bitcoin"
+#     headers = {"Authorization": f"Bearer {api_key}"}
+#     response = requests.get(url, headers=headers)
+#     if response.status_code == 200:
+#         return response.json()["data"]
+#     else:
+#         return None
+
+# # Streamlit interfeysi
+# st.title("Bitcoin Ma'lumotlari")
+# st.write("CoinCap API orqali real vaqt ma'lumotlarini ko'ring")
+
+# # API kalitni kiritish
+# api_key = st.text_input("API kalitingizni kiriting:")
+
+# if api_key:
+#     bitcoin_data = get_bitcoin_data(api_key)
+
+#     if bitcoin_data:
+#         # Ma'lumotlarni jadvalda ko'rsatish
+#         data = {
+#             "Parameter": ["Joriy narx (priceUsd)", "Narx (priceQuote)", "Oxirgi 24 soatlik hajm (volumeUsd24Hr)", 
+#                           "Bozor ulushi o'zgarishi (percentExchangeVolume)", "24 soatlik savdo soni (tradesCount24Hr)", "Oxirgi yangilanish (updated)"],
+#             "Value": [
+#                 f"${float(bitcoin_data['priceUsd']):,.2f}",
+#                 f"${float(bitcoin_data['priceQuote']):,.2f}",
+#                 f"${float(bitcoin_data['volumeUsd24Hr']):,.2f}",
+#                 f"{float(bitcoin_data['percentExchangeVolume']):,.2f}%",
+#                 bitcoin_data['tradesCount24Hr'],
+#                 bitcoin_data['updated']
+#             ]
+#         }
+
+#         df = pd.DataFrame(data)
+#         st.table(df)  # Jadvalda ko'rsatish
+
+#     else:
+#         st.error("Ma'lumotlarni olishda xatolik yuz berdi. API kalitni tekshiring.")
+# else:
+#     st.info("Ma'lumotlarni ko'rish uchun API kalitni kiriting.")
 
 
 
