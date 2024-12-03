1261f3d4fdc21118dc609100f26816905aece650
AlibekSerikbayev <aliserikbek55@gmail.com>
2024-12-03 14:14:32 +0500

    aaa1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import pandas as pd
-
+#uzgarish 3
 # API orqali ma'lumot olish funksiyasi
 def get_bitcoin_data(api_key):
     url = "https://api.coincap.io/v2/assets/bitcoin"
